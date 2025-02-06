d34b9d35446a5fcc0dbdc4802ea296f2bd774645
adrianys75 <adrianysjiemenez@gmail.com>
2025-02-06 09:43:57 -0500

    Add files via upload

---
+++ adri5 carrito.py
@@ -0,0 +1,39 @@
+#adri codigo clase #3
+
+nombre = []
+precio = []
+
+while True : 
+   print("1.añadir")
+   print("2.ver")
+   print("3.eliminar")
+ 
+   a = input("escoje unas de las opciones")
+   if a == "1":
+      a1 = input("escoje un producto")
+      a1.append("nombre")
+      print(f"{nombre}ha sido añadido a la lista")
+      print("1.añadir")
+      
+   elif b == "2":
+      if len(nombre) == 0:
+         print("el carrito de compras esta vacio")
+         print("2.ver")
+      else:
+         print("el carrito de compras contiene:")
+         for i in range(len("nombre")):
+          print(f"{i+1}.{nombres[i]}- ${precio[i]}\n")
+    elif c == "3":
+        if len(nombre) == 0:
+            print("el carrito de compras esta vacio")
+            numero = int(input(" que producto desea eliminar"))
+           c1.pop("nombre")
+            print("elemento pop\n")
+            
+            
+  
+  
+     
+ 
+ 
+    
